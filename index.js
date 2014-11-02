f5112c7daa81a4a46abd1f45ec3e8bdc6d347527
Pierre Inglebert <pierre.inglebert@gmail.com>
2014-11-02 10:46:21 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const joi = require('joi');
+
+module.exports = function (schemas, options) {
+	options = options || {};
+	return function*(next) {
+		try{
+			for(let key in schemas) {
+				yield (function() {
+					return function(cb) {
+						joi.validate(this.request[key], schemas[key], options, cb);
+					};
+				})();
+			}
+		} catch(err) {
+			err.status = 400;
+			throw err;
+		}
+		yield next;
+	};
+};

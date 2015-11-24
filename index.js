8ed79affce57f61b911716d80e26076011e6e6fd
Pierre Inglebert <pierre.inglebert@gmail.com>
2015-11-24 10:39:56 +0100

    use eslint

--- index.js
+++ index.js
@@ -2,22 +2,21 @@
 
 const joi = require('joi');
 
-module.exports = function (schemas, options) {
-	options = options || {};
-	return function*(next) {
-		try{
-			for(let key in schemas) {
-				yield (function() {
-					return function(cb) {
-						joi.validate(this.request[key], schemas[key], options, cb);
-					};
-				})();
-			}
-			
-			yield next;
-		} catch(err) {
-			err.status = 400;
-			throw err;
-		}
-	};
+module.exports = function(schemas, options) {
+  options = options || {};
+  return function*(next) {
+    try {
+      for (let key in schemas) {
+        yield (function() {
+          return function(cb) {
+            joi.validate(this.request[key], schemas[key], options, cb);
+          };
+        })();
+      }
+      yield next;
+    } catch (err) {
+      err.status = 400;
+      throw err;
+    }
+  };
 };
